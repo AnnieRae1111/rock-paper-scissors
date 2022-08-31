e5c2aa3d2e88590641e627f0a2cdc37e2e9146a3
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-08-31 13:55:26 -0600

    game working

--- script.js
+++ script.js
@@ -1,29 +1,94 @@
-let array = ['🤘', '✂️', '📃'];
+let player1Array = ['🤘', '✂️', '📃'];
+let player2Array = ['🤘', '✂️', '📃'];
 const rock = '🤘';
 const paper = '📃';
 const scissors = '✂️';
 let temp;
 let player1;
-let plaarray;
-const onClick = (array) => {
+let player2;
+
+//player1 click
+let button1 = document.getElementById('button1');
+let player1Display = document.querySelector('.player1-rock');
+
+//player2click
+let button2 = document.getElementById('button2');
+let player2Display = document.querySelector('.player2-rock');
+
+const onClick1 = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     const temp = array[i];
     array[i] = array[j];
     array[j] = temp;
-    console.log('temp', temp);
-    return temp;
+    player1 = temp;
   }
 };
 
-console.log(temp);
+const onClick2 = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    player2 = temp;
+  }
+};
 
-let button1 = document.getElementById('button1');
+let player1Wins = document.querySelector('.player1-wins');
+let player2Wins = document.querySelector('.player2-wins');
+let tie = document.querySelector('.tie');
+
+const declareWinner = () => {
+  console.log(player1, player2, 'player1 and 2');
+  console.log(rock, paper, scissors, 'rock paper scissors');
+  if (player1 === '🤘' && player2 === '✂️') {
+    player1Wins.classList.remove('hide');
+  } else if (player1 === '🤘' && player2 === '📃') {
+    player2Wins.classList.remove('hide');
+  } else if (player1 === '🤘' && player2 === '🤘') {
+    tie.classList.remove('hide');
+  } else if (player1 === '✂️' && player2 === '🤘') {
+    player2Wins.classList.remove('hide');
+  } else if (player1 === '✂️' && player2 === '📃') {
+    player1Wins.classList.remove('hide');
+  } else if (player1 === '✂️' && player2 === '✂️') {
+    tie.classList.remove('hide');
+  } else if (player1 === '📃' && player2 === '🤘') {
+    player1Wins.classList.remove('hide');
+  } else if (player1 === '📃' && player2 === '✂️') {
+    player2Wins.classList.remove('hide');
+  } else if (player1 === '📃' && player2 === '📃') {
+    tie.classList.remove('hide');
+  }
+};
+
+const clearWinner = () => {
+  setTimeout(() => {
+    player1Wins.classList.add('hide');
+    player2Wins.classList.add('hide');
+    tie.classList.add('hide');
+    player1Display.classList.add('hide');
+    player2Display.classList.add('hide');
+  }, 2500);
+};
+
 button1.addEventListener('click', () => {
-  onClick(player1);
+  onClick1(player1Array);
+  player1Display.innerHTML = player1;
+  player1Display.classList.remove('hide');
+  console.log(player1, 'player1');
+  declareWinner();
+  clearWinner();
 });
 
-let button2 = document.getElementById('button2');
+//player2 click
+
 button2.addEventListener('click', () => {
-  onClick(player2);
+  onClick2(player2Array);
+  player2Display.innerHTML = player2;
+  player2Display.classList.remove('hide');
+  console.log(player2, 'player2');
+  declareWinner();
+  clearWinner();
 });
