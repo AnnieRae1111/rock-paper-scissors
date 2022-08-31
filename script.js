b254d3889bee2a19cd80bfde0d43dc7821917f14
AnnieRae1111 <annierae.elizabeth@gmail.com>
2022-08-30 20:47:49 -0600

    on click function

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-let player1 = ['🤘', '✂️', '📃'];
-let player2 = ['🤘', '✂️', '📃'];
-
-const onClick = (player) => {
-  for (let i = player.length - 1; i > 0; i--) {
+let array = ['🤘', '✂️', '📃'];
+const rock = '🤘';
+const paper = '📃';
+const scissors = '✂️';
+let temp;
+let player1;
+let plaarray;
+const onClick = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    const temp = player[i];
-    player[i] = player[j];
-    player[j] = temp;
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
     console.log('temp', temp);
     return temp;
   }
 };
+
+console.log(temp);
 
 let button1 = document.getElementById('button1');
 button1.addEventListener('click', () => {
